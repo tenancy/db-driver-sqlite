510017589fa5e6bc1d87397377b75299b91aa6d8
Daniel Klabbers <daniel@klabbers.email>
2018-07-17 15:25:02 +0200

    working on an sqlite driver which should be pretty easy to test

---
+++ Providers/DatabaseProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the tenancy/db-driver-mariadb package.
+ *
+ * (c) Daniël Klabbers <daniel@klabbers.email>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see http://laravel-tenancy.com
+ * @see https://github.com/tenancy
+ */
+
+namespace Tenancy\Database\Drivers\Sqlite\Providers;
+
+use Illuminate\Foundation\Support\Providers\EventServiceProvider;
+use Tenancy\Database\Drivers\Sqlite\Listeners\ConfiguresTenantConnection;
+use Tenancy\Database\Events\Resolving;
+
+class DatabaseProvider extends EventServiceProvider
+{
+    protected $listen = [
+        Resolving::class => [
+            ConfiguresTenantConnection::class
+        ]
+    ];
+
+    public function register()
+    {
+        $this->publishes([__DIR__ . '/../resources/config/db-driver-sqlite.php'], 'db-driver-sqlite');
+
+        $this->mergeConfigFrom(__DIR__ . '/../resources/config/db-driver-sqlite.php', 'db-driver-sqlite');
+    }
+}
